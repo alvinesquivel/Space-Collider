a0f68279b97ed03a296073b744bc998c85caa12f
Alvin_Esquivel <ezkie96@gmail.com>
2015-02-19 22:08:08 +0800

    Assets Directory
    Includes Scripts, GFX, Materials and Scene file.

---
+++ Assets/Scripts/MovingScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingScript : MonoBehaviour {
+
+	float maxSpeed = 5f;
+
+
+	void Update(){
+		Vector3 pos = transform.position;
+		pos.x += Input.GetAxis ("Horizontal") * maxSpeed * Time.deltaTime;
+		pos.y += Input.GetAxis ("Vertical") * maxSpeed * Time.deltaTime;
+		transform.position = pos;
+
+
+	}
+}
---
+++ Assets/Scripts/ShootingScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootingScript : MonoBehaviour {
+
+	public float fireDelay = 0.25f;
+	float cooldownTimer = 0f;
+
+	void Update () {
+		cooldownTimer -= Time.deltaTime;
+
+			if (Input.GetButton ("Fire1") && cooldownTimer <= 0) {
+			{
+				Debug.Log ("Pew!");
+				cooldownTimer = fireDelay;
+			}
+	}
+}
+}
