40a5155568174ae437b59d56bf7b78a8ed84c3a5
Alvin_Esquivel <ezkie96@gmail.com>
2015-02-20 14:52:41 +0800

    Updates

---
+++ Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletMovement : MonoBehaviour {
+
+	float maxSpeed = 10;
+
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		pos.y +=  maxSpeed * Time.deltaTime;
+		transform.position = pos;
+	}
+}
